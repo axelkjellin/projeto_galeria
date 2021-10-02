80cc0a39bb940834e86c74487a790cc6c3d29400
Axel Kjellin <axel@bussolasocial.com.br>
2021-10-02 20:51:21 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+npm start 
+Projeto utilizando jquery, webpack e ajax.
+
+na pasta raiz do projeto
---
+++ src/index.js
@@ -0,0 +1,10 @@
+//importando o sass
+import './scss/index.scss';
+
+//dependencias
+import 'jquery' // se nao passar a extensão do arquivo o webpack ja entende como JS
+import 'bootstrap'
+
+//arquivos js
+import './js/core/includes'
+import './js/plugins/cityButtons'
